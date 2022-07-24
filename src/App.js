425e35ccaa051ff170fc6ab72e8b1b4b1661d9fc
Stewart McFarlane <stewart@toru.digital>
2022-07-24 19:46:51 +0100

    installed tailwind css

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <span>
-          <span>Learn </span>
+          <span className="bg-red-500">Learn </span>
           <a
             className="App-link"
             href="https://reactjs.org/"
