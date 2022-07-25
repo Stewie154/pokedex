a07ef2c027f5cb8e1ebaf213cf156e90b5e982e6
Stewart McFarlane <stewart@toru.digital>
2022-07-25 20:44:45 +0100

    removed unwanted images and added background graphic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
 	return (
-		<div className="container w-full max-w-[700px] h-4/5 p-10 mx-2 sm:mx-10 bg-primary  rounded-lg border-container">
+		<div className="container w-full max-w-[700px] h-4/5 p-10 mx-2 sm:mx-10 bg-primary rounded-lg border-container ">
 			App
 		</div>
 	);
