68eb9c2fcf414ad35be2fe78ad441082df67bbd6
Stewart McFarlane <stewart@toru.digital>
2022-07-25 20:28:01 +0100

    container styles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
 	return (
-		<div className="container mx-auto bg-red-500 h-8/12">
+		<div className="container w-full max-w-[700px] h-4/5 p-10 mx-2 sm:mx-10 bg-primary  rounded-lg border-container">
 			App
 		</div>
 	);
