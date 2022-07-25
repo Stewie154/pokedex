7ac49384c4a6b20d048af1d61782385ef37b3fed
Stewart McFarlane <stewart@toru.digital>
2022-07-25 20:53:32 +0100

    moved images to public folder

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
 	return (
-		<div className="container w-full max-w-[700px] h-4/5 p-10 mx-2 sm:mx-10 bg-primary rounded-lg border-container ">
-			App
+		<div className="container w-full max-w-[700px] h-4/5 p-10 mx-2 sm:mx-10 bg-primary rounded-lg border-container" >
+			<img src='/images/blue-bg-graphic.jpg' className='w-full h-full'/>
 		</div>
 	);
 }
