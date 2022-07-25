34a9e226d14d10fd21ce667ada14d8d4b7ebd2ae
Stewart McFarlane <stewart@toru.digital>
2022-07-25 21:01:43 +0100

    removed comment

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
 	return (
 		<div className="container w-full max-w-[700px] h-4/5 bg-primary p-10 mx-2 sm:mx-10 rounded-lg border-container" >
-			{/* <img src='/images/blue-bg-graphic.jpg' className='w-full h-full'/> */}
+			
 		</div>
 	);
 }
