6d53fbd506f623cf4ae1f7a7e3f64254466fe299
Stewart McFarlane <stewart@toru.digital>
2022-07-26 19:34:50 +0100

    created async thunk to fetch pokemon and console logged them

--- src/redux/slices/pokemonSlice.js
+++ src/redux/slices/pokemonSlice.js
@@ -1,8 +1,12 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios'
 
 const initialState = []
 
 export const fetchPokemon = createAsyncThunk(
 	'pokemonStatus',
-	
+	async () => {
+		let response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+		console.log(response.data)
+	}
 )
--- src/screens/home-screen/HomeScreen.js
+++ src/screens/home-screen/HomeScreen.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPokemon } from '../../redux/slices/pokemonSlice'
 import Header from '../../components/header/Header'
+
 export default function HomeScreen() {
+	
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(fetchPokemon())
+	}, [])
+
 	return (
 		<div>
 			<Header />
