e17897534e058c90b107a69c60b198d1df941ca4
Stewart McFarlane <stewart@toru.digital>
2022-07-29 08:52:44 +0100

    PokemonListItem content

--- src/components/pokemon-list-item/PokemonListItem.js
+++ src/components/pokemon-list-item/PokemonListItem.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const PokemonListItem = ({name, url}) => {
 	return (
-		<div className="flex justify-between items-center">
-			<h2 className="text-3xl">{name}</h2>
-			<img src='/images/pokeball.png'/>
+		<div className="flex justify-between items-center py-3 cursor-pointer hover:border-b border-color-secondary">
+			<h2 className="text-3xl hover:opacity-50">{name}</h2>
+			<img src='/images/pokeball.png' className="w-10"/>
 		</div>
 	)
 }
