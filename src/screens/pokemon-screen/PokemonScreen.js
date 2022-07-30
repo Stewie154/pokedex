014666c63e1ed0c1a340a842f2da5b5d7b5a780b
Stewart McFarlane <stewart@toru.digital>
2022-07-30 10:32:19 +0100

    got pokemon image to render on PokemonScreen

--- src/screens/pokemon-screen/PokemonScreen.js
+++ src/screens/pokemon-screen/PokemonScreen.js
@@ -4,9 +4,11 @@ import { useSelector, useDispatch } from 'react-redux'
 const PokemonScreen = () => {
 	const selected_pokemon = useSelector(store => store.pokemon.selected_pokemon)
 	console.log(selected_pokemon)
+	const image = selected_pokemon.sprites.other.dream_world.front_default
   return (
 	<div>
 		{selected_pokemon.name}
+		<img src={image} />
 	</div>
   )
 }
