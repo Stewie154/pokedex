0c06f6871d9c44920e83b936563911fec14ab453
Stewart McFarlane <stewart@toru.digital>
2022-07-30 14:12:38 +0100

    added type to PokemonScreen

--- src/screens/pokemon-screen/PokemonScreen.js
+++ src/screens/pokemon-screen/PokemonScreen.js
@@ -3,9 +3,17 @@ import { useSelector, useDispatch } from 'react-redux'
 
 const PokemonScreen = () => {
 	const selected_pokemon = useSelector(store => store.pokemon.selected_pokemon)
-	const { name, height, weight } = selected_pokemon
+	const { name, height, weight, types } = selected_pokemon
 	console.log(selected_pokemon)
 	const image = selected_pokemon.sprites.other.dream_world.front_default
+
+	const renderTypes = (types) => {
+		return types.map((type_item, key) => {
+			console.log(type_item)
+			return <p className="mb-3 text-2xl uppercase text-center w-6/12" key={key}>{type_item.type.name}</p>
+		})
+	}
+
 	return (
 		<div>
 			<section className='w-full flex flex-col justify-between items-center mt-5'>
@@ -15,9 +23,12 @@ const PokemonScreen = () => {
 					className="w-full md:w-6/12 pb-[75%] sm:pb-[45%] bg-center bg-contain bg-no-repeat"
 				>
 				</div>
-				<div className='w-full mt-10 flex items-center justify-center'>
-					<p className="text-2xl text-center w-6/12">Height: {height * 10} cm</p>
-					<p className="text-2xl text-center w-6/12">Weight: {weight / 10} kg</p>
+				<div className='w-full mt-10 flex flex-wrap items-center justify-center'>
+					<div className="w-full flex justify-center items-center">
+						{renderTypes(types)}
+					</div>
+					<p className="mb-3 text-2xl text-center w-6/12">Height: {height * 10} cm</p>
+					<p className="mb-3 text-2xl text-center w-6/12">Weight: {weight / 10} kg</p>
 				</div>
 			</section>
 
