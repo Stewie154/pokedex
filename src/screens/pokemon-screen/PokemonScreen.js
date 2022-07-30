02ee37e5081316418a514affe74d11b27d9ea13d
Stewart McFarlane <stewart@toru.digital>
2022-07-30 13:53:19 +0100

    changed image to background image to make them all the same size

--- src/screens/pokemon-screen/PokemonScreen.js
+++ src/screens/pokemon-screen/PokemonScreen.js
@@ -8,12 +8,15 @@ const PokemonScreen = () => {
 	const image = selected_pokemon.sprites.other.dream_world.front_default
   return (
 	<div>
-		<section className='w-full flex justify-between mt-5'>
-			<img src={image} alt={name} className="w-4/12"/>
+		<section className='w-full flex flex-col justify-between items-center mt-5'>
+			<h2 className='capitalize underline text-4xl text-center mb-10'>{name}</h2>
+			<div style={{backgroundImage: `url(${image})`}} className="w-6/12 bg-center bg-contain bg-no-repeat pb-[45%]"></div>
 			<div className='w-7/12'>
-				<h2 className='capitalize underline text-4xl text-center mb-10'>{name}</h2>
-				<p className="text-2xl mb-5">Height: {height * 10} cm</p>
-				<p className="text-2xl">Weight: {weight / 10} kg</p>
+				<div className="">
+					<p className="text-2xl mb-5 w-6/12">Height: {height * 10} cm</p>
+					<p className="text-2xl w-6/12">Weight: {weight / 10} kg</p>
+				</div>
+				
 			</div>
 		</section>
 		
