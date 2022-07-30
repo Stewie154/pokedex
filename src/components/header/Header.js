e1b7804e2c0e498ed6ed87388de86a2cc493a2fb
Stewart McFarlane <stewart@toru.digital>
2022-07-30 14:42:12 +0100

    Header flex layout and border bottom

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
 	return (
-		<div className="w-full h-1/6 flex justify-between items-center">
+		<div className="w-full h-1/6 flex flex-row-reverse justify-between items-center border-b border-color-secondary">
 			<h1 className="text-4xl lg:text-6xl text-center">Pokedex</h1>
 			<img src='/images/filter-outline.svg' alt="filter icon" className="w-10 cursor-pointer hover:opacity-50" />
 		</div>
--- src/screens/pokemon-screen/PokemonScreen.js
+++ src/screens/pokemon-screen/PokemonScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const PokemonScreen = () => {
 	const selected_pokemon = useSelector(store => store.pokemon.selected_pokemon)
