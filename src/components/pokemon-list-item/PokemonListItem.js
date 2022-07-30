087575e5b2ccb5add9006e26f9d1e1afdde88870
Stewart McFarlane <stewart@toru.digital>
2022-07-30 09:21:09 +0100

    spin pokeball icon on PokemonListItem hover

--- src/components/pokemon-list-item/PokemonListItem.js
+++ src/components/pokemon-list-item/PokemonListItem.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const PokemonListItem = ({name, url}) => {
+	const [hovering, setHovering] = useState(false)
+	const handleHover = () => {
+		setHovering(!hovering)
+	}
 	return (
-		<div className="flex justify-between items-center py-3 cursor-pointer hover:border-b border-color-secondary">
+		<div 
+			className="flex justify-between items-center py-3 cursor-pointer hover:border-b border-color-secondary" 
+			onMouseEnter={() => handleHover()}
+			onMouseLeave={() => handleHover()}
+		>
 			<h2 className="text-3xl hover:opacity-50">{name}</h2>
-			<img src='/images/pokeball.png' className="w-10" alt="pokeball"/>
+			<img src='/images/pokeball.png' className={`w-10 ${hovering && 'animate-spin'}`} alt="pokeball"/>
 		</div>
 	)
 }
