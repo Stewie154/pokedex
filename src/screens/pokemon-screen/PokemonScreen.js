1791c6444c75d7e8d20f56392911662344cf7092
Stewart McFarlane <stewart@toru.digital>
2022-07-30 11:10:35 +0100

    destructured props

--- src/screens/pokemon-screen/PokemonScreen.js
+++ src/screens/pokemon-screen/PokemonScreen.js
@@ -3,16 +3,17 @@ import { useSelector, useDispatch } from 'react-redux'
 
 const PokemonScreen = () => {
 	const selected_pokemon = useSelector(store => store.pokemon.selected_pokemon)
+	const {name, height, weight} = selected_pokemon
 	console.log(selected_pokemon)
 	const image = selected_pokemon.sprites.other.dream_world.front_default
   return (
 	<div>
 		<section className='w-full flex justify-between mt-5'>
-			<img src={image} alt={selected_pokemon.name} className="w-4/12"/>
+			<img src={image} alt={name} className="w-4/12"/>
 			<div className='w-7/12'>
-				<h2 className='capitalize underline text-4xl text-center mb-10'>{selected_pokemon.name}</h2>
-				<p className="text-2xl mb-5">Height: {selected_pokemon.height * 10} cm</p>
-				<p className="text-2xl">Weight: {selected_pokemon.weight / 10} kg</p>
+				<h2 className='capitalize underline text-4xl text-center mb-10'>{name}</h2>
+				<p className="text-2xl mb-5">Height: {height * 10} cm</p>
+				<p className="text-2xl">Weight: {weight / 10} kg</p>
 			</div>
 		</section>
 		
