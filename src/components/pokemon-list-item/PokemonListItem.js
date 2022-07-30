8d7a48ade9bd33358bcb7b0cced1cfff65a07959
Stewart McFarlane <stewart@toru.digital>
2022-07-30 10:50:19 +0100

    useNavigate to redirect after action has finished

--- src/components/pokemon-list-item/PokemonListItem.js
+++ src/components/pokemon-list-item/PokemonListItem.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchSinglePokemon } from '../../redux/slices/pokemonSlice'
-import { Link } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 const PokemonListItem = ({ name, url }) => {
 	const [hovering, setHovering] = useState(false)
+
+	const navigate = useNavigate();
 
 	const dispatch = useDispatch()
 
@@ -11,23 +13,21 @@ const PokemonListItem = ({ name, url }) => {
 		setHovering(!hovering)
 	}
 
-	const handleClick = (url) => {
-		dispatch(fetchSinglePokemon(url))
+	const handleClick = async (url, name) => {
+		await dispatch(fetchSinglePokemon(url))
+		navigate(`/${name}`);
 	}
 
 	return (
-		<Link to={`/${name}`}>
-			<div
-				className="flex justify-between items-center py-3 cursor-pointer hover:border-b border-color-secondary"
-				onMouseEnter={() => handleHover()}
-				onMouseLeave={() => handleHover()}
-				onClick={() => handleClick(url)}
-			>
-				<h2 className="text-3xl hover:opacity-50">{name}</h2>
-				<img src='/images/pokeball.png' className={`w-10 ${hovering && 'animate-spin'}`} alt="pokeball" />
-			</div>
-		</Link>
-
+		<div
+			className="flex justify-between items-center py-3 cursor-pointer hover:border-b border-color-secondary"
+			onMouseEnter={() => handleHover()}
+			onMouseLeave={() => handleHover()}
+			onClick={() => handleClick(url, name)}
+		>
+			<h2 className="text-3xl hover:opacity-50">{name}</h2>
+			<img src='/images/pokeball.png' className={`w-10 ${hovering && 'animate-spin'}`} alt="pokeball" />
+		</div>
 	)
 }
 
