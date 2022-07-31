946b5ba07310e0ab227dfd281b0ac96fd780055e
Stewart McFarlane <stewart@toru.digital>
2022-07-31 20:55:00 +0100

    created Pagination component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import RouterComponent from '../router/RouterComponent'
+import Pagination from '../pagination/Pagination';
 
 const App = () => {
 	return (
 		<div className="container w-full max-w-[700px] h-[80vh] bg-primary px-10 mx-2 sm:mx-10 rounded-lg border-container">
 			<RouterComponent/>
+			<Pagination />
 		</div>
 	);
 }
---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pagination = () => {
+  return (
+	<div className="h-1/6 flex justify-around items-center">
+		<p>Prev</p>
+		<p>Next</p>
+	</div>
+  )
+}
+
+export default Pagination
