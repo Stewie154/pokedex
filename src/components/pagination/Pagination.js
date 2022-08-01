84beda5c036aa1e2ed146bd248b362abdb7fb852
Stewart McFarlane <stewart@toru.digital>
2022-08-01 06:42:04 +0100

    button hover styles

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Pagination = () => {
   return (
 	<div className="h-1/6 flex justify-around items-center">
-		<button className="bg-secondary text-lg px-7 py-2 border rounded-lg">Prev</button>
-		<button className="bg-secondary text-lg px-7 py-2 border rounded-lg">Next</button>
+		<button className="bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400">Prev</button>
+		<button className="bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400">Next</button>
 	</div>
   )
 }
