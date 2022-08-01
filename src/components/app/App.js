cc8a2ec39cb486f836277083a15d0be69acf824b
Stewart McFarlane <stewart@toru.digital>
2022-08-01 07:47:16 +0100

    removed import

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import RouterComponent from '../router/RouterComponent'
-import Pagination from '../pagination/Pagination';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchPokemonList } from '../../redux/slices/pokemonSlice';
@@ -14,6 +13,7 @@ const App = () => {
 	return (
 		<div className="container w-full max-w-[700px] h-[80vh] bg-primary px-10 mx-2 sm:mx-10 rounded-lg border-container">
 			<RouterComponent />
+			
 		</div>
 	);
 }
