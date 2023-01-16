6a9fb3e61002daa1eec8293bd46768084d312daa
Stewart McFarlane <stewart@toru.digital>
2023-01-16 08:28:16 +0000

    reduced size of content in PokemonListItem

--- src/components/pokemon-list-item/PokemonListItem.js
+++ src/components/pokemon-list-item/PokemonListItem.js
@@ -20,13 +20,13 @@ const PokemonListItem = ({ name, url }) => {
 
 	return (
 		<div
-			className="flex justify-between items-center py-3 cursor-pointer hover:border-b border-color-secondary"
+			className="flex justify-between items-center py-2 cursor-pointer hover:border-b border-color-secondary"
 			onMouseEnter={() => handleHover()}
 			onMouseLeave={() => handleHover()}
 			onClick={() => handleClick(url, name)}
 		>
-			<h2 className="text-3xl capitalize hover:opacity-50">{name}</h2>
-			<img src='/images/pokeball.png' className={`w-10 transition ease-in-out delay-150 ${hovering && 'pokeball-animation'}`} alt="pokeball" />
+			<h2 className="text-xl lg:text-3xl capitalize hover:opacity-50">{name}</h2>
+			<img src='/images/pokeball.png' className={`w-7 transition ease-in-out delay-150 ${hovering && 'pokeball-animation'}`} alt="pokeball" />
 		</div>
 	)
 }
