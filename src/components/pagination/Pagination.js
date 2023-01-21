482e7368a75a7f38059c71eabd62823acb33dadb
Stewart McFarlane <stewart@toru.digital>
2023-01-21 21:17:56 +0000

    hide prev button when no previous pokemon are available

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -5,6 +5,7 @@ import { displayNextList, displayPreviousList } from '../../redux/slices/pokemon
 const Pagination = () => {
 	const dispatch = useDispatch()
 	const pokemon_list = useSelector(state => state.pokemon.pokemon_list)
+	const first_pokemon_being_displayed = useSelector(state => state.pokemon.first_pokemon_being_displayed)
 	const last_pokemon_being_displayed = useSelector(state => state.pokemon.last_pokemon_being_displayed)
 	
 
@@ -19,13 +20,13 @@ const Pagination = () => {
 	return (
 		<div className="h-[10%] flex justify-around items-center">
 			<button 
-				className={`bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400`}
+				className={`${pokemon_list[0] == first_pokemon_being_displayed ? 'hidden' : 'bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400'}`}
 				onClick={() => handlePrevBtnClick()}
 			>
 				Prev
 			</button>
 			<button 
-				className={`${pokemon_list[pokemon_list.length -1] == last_pokemon_being_displayed ? 'hidden' : 'bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400'} `}
+				className={`${pokemon_list[pokemon_list.length -1] == last_pokemon_being_displayed ? 'hidden' : 'bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400'}`}
 				onClick={() => handleNextBtnClick()}
 			>
 				Next
