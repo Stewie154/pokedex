4a5746ef729fee4e396f61bb5fd649e1136096bb
Stewart McFarlane <stewart@toru.digital>
2023-01-22 17:36:31 +0000

    button colour change on theme toggle

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { displayNextList, displayPreviousList } from '../../redux/slices/pokemonSlice'
 
 const Pagination = () => {
+	const theme = useSelector(state => state.pokemon.theme)
 	const dispatch = useDispatch()
 	const pokemon_list = useSelector(state => state.pokemon.pokemon_list)
 	const first_pokemon_being_displayed = useSelector(state => state.pokemon.first_pokemon_being_displayed)
@@ -20,13 +21,13 @@ const Pagination = () => {
 	return (
 		<div className="h-[10%] flex justify-around items-center">
 			<button 
-				className={`${pokemon_list[0] === first_pokemon_being_displayed ? 'hidden' : 'light-mode-bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400'}`}
+				className={`${pokemon_list[0] === first_pokemon_being_displayed ? 'hidden' : `${theme}-mode-bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400`}`}
 				onClick={() => handlePrevBtnClick()}
 			>
 				Prev
 			</button>
 			<button 
-				className={`${pokemon_list[pokemon_list.length -1] === last_pokemon_being_displayed ? 'hidden' : 'light-mode-bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400'}`}
+				className={`${pokemon_list[pokemon_list.length -1] === last_pokemon_being_displayed ? 'hidden' : `${theme}-mode-bg-secondary text-lg font-bold px-7 py-2 border rounded-lg transition-all duration-400`}`}
 				onClick={() => handleNextBtnClick()}
 			>
 				Next
