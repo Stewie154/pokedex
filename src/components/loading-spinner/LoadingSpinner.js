00cef817a11ae6315c027311c2e3f6e408d3469c
Stewart McFarlane <stewart@toru.digital>
2023-01-22 17:45:59 +0000

    theme-coloured loading spinner

--- src/components/loading-spinner/LoadingSpinner.js
+++ src/components/loading-spinner/LoadingSpinner.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import './LoadingSpinner.scss'
 
 const LoadingSpinner = () => {
-  return (
-	<div className="spinner-container absolute top-0 right-0">
-		<div className="loading-spinner">
+	const theme = useSelector(state => state.pokemon.theme)
+	return (
+		<div className="spinner-container absolute top-0 right-0">
+			<div className={`loading-spinner ${theme}-mode-spinner`}>
+			</div>
 		</div>
-	</div>
-  )
+	)
 }
 
 export default LoadingSpinner
