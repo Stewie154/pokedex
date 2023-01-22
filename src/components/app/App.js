14329b167d8e1cc7858863ca63d58158efedb71f
Stewart McFarlane <stewart@toru.digital>
2023-01-22 17:25:10 +0000

    toggle theme classes in App on theme icon click

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import RouterComponent from '../router/RouterComponent'
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchPokemonList } from '../../redux/slices/pokemonSlice';
 import FormComponent from '../form-component/FormComponent';
 
 const App = () => {
+	const theme = useSelector(state => state.pokemon.theme)
+
 	const dispatch = useDispatch()
 
 	useEffect(() => {
 		dispatch(fetchPokemonList())
-		document.body.classList.add('light-mode-bg-gradient')
-	}, [dispatch])
+		document.body.classList.add(`${theme}-mode-bg-gradient`)
+	}, [dispatch, theme])
 	return (
-		<div className="container w-full max-w-[700px] h-[90vh] light-mode-bg-primary px-10 mx-2 sm:mx-10 rounded-lg light-mode-border-container">
+		<div className={`container w-full max-w-[700px] h-[90vh] ${theme}-mode-bg-primary px-10 mx-2 sm:mx-10 rounded-lg ${theme}-mode-border-container`}>
 			<RouterComponent />
 			{/* <FormComponent /> */}
 		</div>
