9e91f531452a3d30092947d08d1f55086619e8c9
Stewart McFarlane <stewart@toru.digital>
2023-01-22 17:17:55 +0000

    render light or dark icon based off theme state

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import DarkModeIcon from '../icon-components/dark-mode-icon/DarkModeIcon'
+import LightModeIcon from '../icon-components/light-mode-icon/LightModeIcon'
 
 const Header = () => {
+
+	const theme = useSelector(state => state.pokemon.theme)
+	const theme_icon = theme === 'light' ? <DarkModeIcon /> : <LightModeIcon />
 
 	return (
 		<div className="w-full h-[10%] flex flex-row-reverse justify-between items-center border-b light-mode-border-color-secondary">
 			<h1 className="text-2xl lg:text-4xl text-center">Pokedex</h1>
-			<DarkModeIcon />
+			{theme_icon}
 		</div>
 	)
 }
