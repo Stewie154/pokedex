664f89c098b0d79b26d27b357786d9d2cd7e3dd2
Stewart McFarlane <stewart@toru.digital>
2023-02-05 17:10:40 +0000

    added fade animation to pokemon list

--- src/components/pokemon-list-item/PokemonListItem.js
+++ src/components/pokemon-list-item/PokemonListItem.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import Fade from 'react-reveal/Fade';
 import { fetchSinglePokemon } from '../../redux/slices/pokemonSlice'
 import { useNavigate } from "react-router-dom";
 const PokemonListItem = ({ name, url }) => {
@@ -20,16 +21,18 @@ const PokemonListItem = ({ name, url }) => {
 	}
 
 	return (
-		<div
-			className={`relative flex justify-between items-center py-2 cursor-pointer group transition-all duration-300`}
-			onMouseEnter={() => handleHover()}
-			onMouseLeave={() => handleHover()}
-			onClick={() => handleClick(url, name)}
-		>
-			<h2 className="text-xl lg:text-3xl capitalize hover:opacity-50">{name}</h2>
-			<img src={`/images/${theme}-mode-ball.png`} className={`w-7 transition ease-in-out delay-150 ${hovering && 'pokeball-animation'}`} alt="pokeball" />
-			<div className={`absolute bottom-0 h-px w-0 ${theme}-mode-border-color-secondary group-hover:w-full group-hover:border-b transition-all duration-500`}></div>
-		</div>
+		<Fade>
+			<div
+				className={`relative flex justify-between items-center py-2 cursor-pointer group transition-all duration-300`}
+				onMouseEnter={() => handleHover()}
+				onMouseLeave={() => handleHover()}
+				onClick={() => handleClick(url, name)}
+			>
+				<h2 className="text-xl lg:text-3xl capitalize hover:opacity-50">{name}</h2>
+				<img src={`/images/${theme}-mode-ball.png`} className={`w-7 transition ease-in-out delay-150 ${hovering && 'pokeball-animation'}`} alt="pokeball" />
+				<div className={`absolute bottom-0 h-px w-0 ${theme}-mode-border-color-secondary group-hover:w-full group-hover:border-b transition-all duration-500`}></div>
+			</div>
+		</Fade>
 	)
 }
 
