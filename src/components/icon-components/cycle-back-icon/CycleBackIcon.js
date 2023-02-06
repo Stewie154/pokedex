02451a5fd37fa372a34d94e3aea9bcdef029f403
Stewart McFarlane <stewart@toru.digital>
2023-02-06 08:54:43 +0000

    rendered theme colour arrow in CycleBackIcon

--- src/components/icon-components/cycle-back-icon/CycleBackIcon.js
+++ src/components/icon-components/cycle-back-icon/CycleBackIcon.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const CycleBackIcon = () => {
+	const theme = useSelector(state => state.pokemon.theme)
+
+	const dispatch = useDispatch()
+
+	const handleClick = () => {
+		// dispatch()
+		console.log('click handler')
+	}
 	return (
-		<div>CycleBackIcon</div>
+		<img
+			src={`/images/icons/arrow-${theme}-mode.svg`}
+			alt="filter icon"
+			className="w-10 bottom-0 absolute cursor-pointer hover:opacity-50 transition-all duration-300"
+			onClick={() => handleClick()}
+		/>
 	)
 }
 
