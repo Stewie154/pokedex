49c54d225fb8563d4180b0ba7173f78143d37314
Stewart McFarlane <stewart@toru.digital>
2023-02-06 09:08:05 +0000

    flex item back icon

--- src/screens/pokemon-screen/PokemonScreen.js
+++ src/screens/pokemon-screen/PokemonScreen.js
@@ -25,15 +25,15 @@ const PokemonScreen = () => {
 	}
 
 	return (
-		<div className="relative h-[90%] flex flex-col overflow-scroll pt-5">
-			<BackIcon />
-			<section className="w-full flex flex-row-reverse justify-between mt-14">
+		<div className="relative h-[90%] flex flex-col overflow-scroll pt-3">
+			<section className="w-full flex flex-row-reverse justify-between">
 				<div
 					style={{ backgroundImage: `url(${image})` }}
 					className="w-full md:w-6/12 pb-[75%] sm:pb-[50%] bg-center bg-contain bg-no-repeat"
 				>
 				</div>
-				<div className="flex flex-grow flex-col justify-between">
+				<div className="flex flex-grow flex-col justify-start">
+					<BackIcon />
 					<h2 className="capitalize underline text-4xl text-left mb-7">{name}</h2>
 					<div className="w-full flex items-center">
 						{renderTypes(types)}
