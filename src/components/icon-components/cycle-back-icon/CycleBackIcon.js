fc4ac9641445718cc5a5abc3e2dbd64b06d5c79e
Stewart McFarlane <stewart@toru.digital>
2023-02-07 09:01:46 +0000

    cycle previous pokemon working

--- src/components/icon-components/cycle-back-icon/CycleBackIcon.js
+++ src/components/icon-components/cycle-back-icon/CycleBackIcon.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { fetchSinglePokemon } from '../../../redux/slices/pokemonSlice'
+import { useNavigate } from "react-router-dom";
 
 
 const CycleBackIcon = () => {
 	const theme = useSelector(state => state.pokemon.theme)
-
+	const pokemon_list = useSelector(state => state.pokemon.pokemon_list)
+	const selected_pokemon = useSelector(state => state.pokemon.selected_pokemon)
 	const dispatch = useDispatch()
+	const navigate = useNavigate();
 
-	const handleClick = () => {
-		// dispatch()
-		console.log('click handler')
+	const handleClick = async () => {
+		let current_list_item = pokemon_list.find(pokemon => pokemon.name === selected_pokemon.name)
+		let current_list_item_index = pokemon_list.indexOf(current_list_item)
+		let previous_pokemon = pokemon_list[current_list_item_index - 1]
+		if (previous_pokemon) {
+			await dispatch(fetchSinglePokemon(previous_pokemon.url))
+			navigate(`/${previous_pokemon.name}`)
+		}
 	}
+	
 	return (
 		<img
 			src={`/images/icons/arrow-${theme}-mode.svg`}
