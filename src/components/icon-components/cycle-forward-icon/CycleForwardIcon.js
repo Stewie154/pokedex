1c5052f513b2704e355b60269740f71ecd8476b8
Stewart McFarlane <stewart@toru.digital>
2023-02-07 08:49:19 +0000

    get the index of the currently selected pokemon from pokemon_list

--- src/components/icon-components/cycle-forward-icon/CycleForwardIcon.js
+++ src/components/icon-components/cycle-forward-icon/CycleForwardIcon.js
@@ -2,14 +2,19 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 
+
 const CycleForwardIcon = () => {
 	const theme = useSelector(state => state.pokemon.theme)
-
+	const pokemon_list = useSelector(state => state.pokemon.pokemon_list)
+	const selected_pokemon = useSelector(state => state.pokemon.selected_pokemon)
 	const dispatch = useDispatch()
 
 	const handleClick = () => {
 		// dispatch()
-		console.log('click handler')
+		let current_list_item = pokemon_list.find(pokemon => pokemon.name === selected_pokemon.name)
+		let current_list_item_index = pokemon_list.indexOf(current_list_item)
+		console.log(current_list_item_index)
+		
 	}
 	return (
 		<img
