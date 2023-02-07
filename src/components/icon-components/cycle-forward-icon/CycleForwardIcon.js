4b80f2b280422021166de518c059290f3c80dd62
Stewart McFarlane <stewart@toru.digital>
2023-02-07 08:57:38 +0000

    click handler for cycling forwards through pokemon working

--- src/components/icon-components/cycle-forward-icon/CycleForwardIcon.js
+++ src/components/icon-components/cycle-forward-icon/CycleForwardIcon.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
-
+import { fetchSinglePokemon } from '../../../redux/slices/pokemonSlice'
+import { useNavigate } from "react-router-dom";
 
 const CycleForwardIcon = () => {
 	const theme = useSelector(state => state.pokemon.theme)
 	const pokemon_list = useSelector(state => state.pokemon.pokemon_list)
 	const selected_pokemon = useSelector(state => state.pokemon.selected_pokemon)
 	const dispatch = useDispatch()
+	const navigate = useNavigate();
 
 	const handleClick = async () => {
-		// dispatch()
 		let current_list_item = pokemon_list.find(pokemon => pokemon.name === selected_pokemon.name)
 		let current_list_item_index = pokemon_list.indexOf(current_list_item)
 		let next_pokemon = pokemon_list[current_list_item_index + 1]
-	
+		if (next_pokemon) {
+			await dispatch(fetchSinglePokemon(next_pokemon.url))
+			navigate(`/${next_pokemon.name}`)
+		}
 	}
+	
 	return (
 		<img
 			src={`/images/icons/arrow-${theme}-mode.svg`}
