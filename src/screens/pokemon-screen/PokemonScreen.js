994f8e1e9fe21df3b4c0aed8b9cf0dee8316871e
Stewart McFarlane <stewart@toru.digital>
2023-02-08 08:30:52 +0000

    fade in pokemon screen

--- src/screens/pokemon-screen/PokemonScreen.js
+++ src/screens/pokemon-screen/PokemonScreen.js
@@ -4,6 +4,7 @@ import BackIcon from '../../components/icon-components/back-icon/BackIcon'
 import CycleBackIcon from '../../components/icon-components/cycle-back-icon/CycleBackIcon'
 import CycleForwardIcon from '../../components/icon-components/cycle-forward-icon/CycleForwardIcon'
 import LoadingSpinner from '../../components/loading-spinner/LoadingSpinner'
+import { Fade } from 'react-reveal'
 
 const PokemonScreen = () => {
 	const is_loading = useSelector(state => state.pokemon.is_loading)
@@ -30,38 +31,42 @@ const PokemonScreen = () => {
 	return (
 		<>
 			{is_loading && <LoadingSpinner />}
-			<div className={`${is_loading ? 'hidden' : 'relative h-[90%] flex flex-col overflow-scroll pt-5'}`}>
-				<section className="w-full flex flex-row-reverse justify-between">
-					<div
-						style={{ backgroundImage: `url(${image})` }}
-						className="w-full md:w-6/12 pb-[75%] sm:pb-[55%] bg-center bg-contain bg-no-repeat"
-					>
-					</div>
-					<div className="flex flex-grow flex-col justify-start">
-						<BackIcon />
-						<div>
-							<h2 className="capitalize underline text-4xl text-left mb-6">{name}</h2>
-							<div className="flex items-center">
-								{renderTypes(types)}
+			<Fade>
+				<div className={`${is_loading ? 'hidden' : 'relative h-[90%] flex flex-col overflow-scroll pt-5'}`}>
+					<section className="w-full flex flex-row-reverse justify-between">
+
+						<div
+							style={{ backgroundImage: `url(${image})` }}
+							className="w-full md:w-6/12 pb-[75%] sm:pb-[55%] bg-center bg-contain bg-no-repeat"
+						>
+						</div>
+
+						<div className="flex flex-grow flex-col justify-start">
+							<BackIcon />
+							<div>
+								<h2 className="capitalize underline text-4xl text-left mb-6">{name}</h2>
+								<div className="flex items-center">
+									{renderTypes(types)}
+								</div>
+								<p className="mb-3 sm:text-xl text-left w-full">Height: {height * 10} cm</p>
+								<p className="mb-3 sm:text-xl text-left w-full">Weight: {weight / 10} kg</p>
 							</div>
-							<p className="mb-3 sm:text-xl text-left w-full">Height: {height * 10} cm</p>
-							<p className="mb-3 sm:text-xl text-left w-full">Weight: {weight / 10} kg</p>
 						</div>
-					</div>
-				</section>
-				<section className={`w-full mt-5 flex-grow flex flex-col justify-evenly border-t ${theme}-mode-border-color-secondary`}>
-					<div>
-						<h3 className="w-full capitalize text-2xl sm:text-3xl text-center mb-5">Stats</h3>
-						<div className={`w-full grid grid-cols-2 md:grid-cols-3 gap-3`}>
-							{renderStats(stats)}
+					</section>
+					<section className={`w-full mt-5 flex-grow flex flex-col justify-evenly border-t ${theme}-mode-border-color-secondary`}>
+						<div>
+							<h3 className="w-full capitalize text-2xl sm:text-3xl text-center mb-5">Stats</h3>
+							<div className={`w-full grid grid-cols-2 md:grid-cols-3 gap-3`}>
+								{renderStats(stats)}
+							</div>
 						</div>
-					</div>
-					<div className="w-full flex justify-center items-center">
-						<CycleBackIcon />
-						<CycleForwardIcon />
-					</div>
-				</section>
-			</div>
+						<div className="w-full flex justify-center items-center">
+							<CycleBackIcon />
+							<CycleForwardIcon />
+						</div>
+					</section>
+				</div>
+			</Fade>
 		</>
 	)
 }
