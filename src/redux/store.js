750e21460b38b219c957467e6e69ff7230e7e8fa
Stewart McFarlane <stewart@toru.digital>
2023-02-09 09:00:49 +0000

    configured redux persist to save redux store data to local storage (stop page breaking on refresh)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import pokemonReducer from './slices/pokemonSlice'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, pokemonReducer)
+
+
 export const store = configureStore({
 	reducer: {
-		pokemon: pokemonReducer
+		pokemon: persistedReducer
 	},
+	middleware: [thunk]
 });
+
+export const persistor = persistStore(store)
