3f876d282e4eebe35d5730ca54b7f196c8d5eeb2
Stewart McFarlane <stewart@toru.digital>
2023-02-13 09:24:35 +0000

    added select tag

--- src/components/pokemon-list-selector/PokemonListSelector.js
+++ src/components/pokemon-list-selector/PokemonListSelector.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PokemonListSelector = () => {
+	const [selectedValue, setSelectedValue] = useState("1")
 	return (
-		<div>PokemonListSelector</div>
+		<select value={selectedValue}>
+			<option value="1">Generation 1</option>
+			<option value="2">Generation 2</option>
+			<option value="3">Generation 3</option>
+			<option value="4">Generation 4</option>
+		</select>
 	)
 }
 
