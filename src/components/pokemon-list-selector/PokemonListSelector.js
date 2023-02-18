117798c7adef2b2cc05464307f2ea079d6d2297f
Stewart McFarlane <stewart@toru.digital>
2023-02-18 12:00:55 +0000

    handleChange function for PokemonListSelector

--- src/components/pokemon-list-selector/PokemonListSelector.js
+++ src/components/pokemon-list-selector/PokemonListSelector.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
+import { fetchPokemonList } from '../../redux/slices/pokemonSlice'
+import { useDispatch } from 'react-redux'
 
 const PokemonListSelector = () => {
+	const dispatch = useDispatch()
 	const [selectedValue, setSelectedValue] = useState("1")
 
+	const handleChange = (generation) => {
+		setSelectedValue(generation)
+	}
 	
 	return (
-		<select value={selectedValue} className={`bg-transparent text-xl cursor-pointer focus:outline-0`}>
+		<select value={selectedValue} className={`bg-transparent text-xl cursor-pointer focus:outline-0`} onChange={(e) => {handleChange(e.target.value)}}>
 			<option value="1">Generation 1</option>
 			<option value="2">Generation 2</option>
 		</select>
