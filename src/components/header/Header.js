4ec83f007786443be233741d5e1e3a0f5c0badf4
Stewart McFarlane <stewart@toru.digital>
2023-02-18 11:39:44 +0000

    rendered PokemonListSelector in Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import DarkModeIcon from '../icon-components/dark-mode-icon/DarkModeIcon'
 import LightModeIcon from '../icon-components/light-mode-icon/LightModeIcon'
+import PokemonListSelector from '../pokemon-list-selector/PokemonListSelector'
 
 const Header = () => {
 
@@ -11,6 +12,7 @@ const Header = () => {
 	return (
 		<div className={`w-full h-[10%] flex flex-row-reverse justify-between items-center border-b ${theme}-mode-border-color-secondary transition-all duration-300`}>
 			<h1 className="text-2xl lg:text-4xl text-center">Pokedex</h1>
+			<PokemonListSelector />
 			{theme_icon}
 		</div>
 	)
