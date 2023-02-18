de1ac4035f6de24e1f0a7db34e4f1b8c23ab6752
Stewart McFarlane <stewart@toru.digital>
2023-02-18 12:32:02 +0000

    handle selecting a different generation from PokemonScreen

--- src/components/pokemon-list-selector/PokemonListSelector.js
+++ src/components/pokemon-list-selector/PokemonListSelector.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react'
-import { fetchPokemonList, selectGeneration } from '../../redux/slices/pokemonSlice'
+import { fetchPokemonList, selectGeneration, removeSelectedPokemon } from '../../redux/slices/pokemonSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from "react-router-dom";
+
 
 const PokemonListSelector = () => {
 	const dispatch = useDispatch()
+	const navigate = useNavigate()
 	const generation = useSelector(state => state.pokemon.generation)
+	const selected_pokemon = useSelector(state => state.pokemon.selected_pokemon)
 
 	const handleChange = (generation) => {
 		dispatch(selectGeneration(generation))
@@ -12,6 +16,10 @@ const PokemonListSelector = () => {
 
 	useEffect(() => {
 		dispatch(fetchPokemonList(generation))
+		if (selected_pokemon) {
+			dispatch(removeSelectedPokemon())
+			navigate("/")
+		}
 	}, [generation])
 	
 	return (
