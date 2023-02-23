ea9f096affa7105781caf9bed213796b1861abf0
Stewart McFarlane <stewart@toru.digital>
2023-02-23 09:04:44 +0000

    added text to ErrorScreen

--- src/screens/error-screen/ErrorScreen.js
+++ src/screens/error-screen/ErrorScreen.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const ErrorScreen = () => {
+	const error = useSelector(state => state.pokemon.error)
+
 	return (
-		<div className="w-full h-full flex flex-col items-center justify-center">
+		<div className="w-full h-full py-5 flex flex-col items-center justify-center">
+			<h1 className="text-3xl md:text-5xl pb-3">An Error Occurred</h1>
+			<h3 className="text-xl md:text-3xl">Name: {error}</h3>
 			<img 
 				className="w-8/12"
 				src="/images/pikachu-meme.png"
