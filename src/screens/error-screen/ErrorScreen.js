87e63299e0e82ec7cc55ce28a728a4ee2549e889
Stewart McFarlane <stewart@toru.digital>
2023-02-23 09:00:17 +0000

    rendered error screen image

--- src/screens/error-screen/ErrorScreen.js
+++ src/screens/error-screen/ErrorScreen.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ErrorScreen = () => {
 	return (
-		<div className="w-full h-full">ErrorScreen</div>
+		<div className="w-full h-full flex flex-col items-center justify-center">
+			<img 
+				className="w-8/12"
+				src="/images/pikachu-meme.png"
+				alt="pikachu" 
+			/>
+		</div>
 	)
 }
 
