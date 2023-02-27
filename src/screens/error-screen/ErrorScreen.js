58f3157ed737b1034ce21b54923f59731ebc7b15
Stewart McFarlane <stewart@toru.digital>
2023-02-27 08:47:10 +0000

    added Fade animation to ErrorScreen

--- src/screens/error-screen/ErrorScreen.js
+++ src/screens/error-screen/ErrorScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Fade } from 'react-reveal';
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchPokemonList, removeSelectedPokemon } from '../../redux/slices/pokemonSlice'
 import { useNavigate } from "react-router-dom";
@@ -11,35 +12,37 @@ const ErrorScreen = () => {
 	const selected_pokemon = useSelector(state => state.pokemon.selected_pokemon)
 	const dispatch = useDispatch()
 
-	
+
 	useEffect(() => {
 		if (error === null) {
 			navigate("/")
 		}
 	}, [error])
 
-	const handleRefreshClick =  () => {
+	const handleRefreshClick = () => {
 		if (selected_pokemon) {
 			dispatch(removeSelectedPokemon())
-		}	
+		}
 		dispatch(fetchPokemonList())
 	}
 
 	return (
-		<div className="w-full h-full py-5 flex flex-col items-center justify-center">
-			<h1 className="text-3xl md:text-5xl pb-3">An Error Occurred</h1>
-			<h3 className="text-xl md:text-3xl">Name: {error}</h3>
-			<img 
-				className="w-10/12 sm:w-8/12"
-				src="/images/pikachu-meme.png"
-				alt="pikachu" 
-			/>
-			<img
-				className="w-[60px] md:hover:opacity-70 cursor-pointer"
-				src={`/images/icons/refresh-${theme}-mode.svg`}
-				onClick={() => handleRefreshClick()}
-			/>
-		</div>
+		<Fade>
+			<div className="w-full h-full py-5 flex flex-col items-center justify-center">
+				<h1 className="text-3xl md:text-5xl pb-3">An Error Occurred</h1>
+				<h3 className="text-xl md:text-3xl">Name: {error}</h3>
+				<img
+					className="w-10/12 sm:w-8/12"
+					src="/images/pikachu-meme.png"
+					alt="pikachu"
+				/>
+				<img
+					className="w-[60px] md:hover:opacity-70 cursor-pointer"
+					src={`/images/icons/refresh-${theme}-mode.svg`}
+					onClick={() => handleRefreshClick()}
+				/>
+			</div>
+		</Fade>
 	)
 }
 
